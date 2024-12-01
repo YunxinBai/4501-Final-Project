039810dcc903c8ae659ad7de3429660476d86304
YunxinBai <yb2609@columbia.edu>
2024-12-01 18:25:03 -0500

    Update the markdown to notebook and update the weather data

--- schema.sql
+++ schema.sql
@@ -1,36 +1,55 @@
 
 CREATE TABLE IF NOT EXISTS hourly_weather (
-    datetime TEXT NOT NULL,
-    temperature FLOAT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date DATETIME NOT NULL,
     precipitation FLOAT,
     wind_speed FLOAT
 );
 
 CREATE TABLE IF NOT EXISTS daily_weather (
-    date TEXT NOT NULL,
-    avg_precipitation FLOAT,
-    avg_wind_speed FLOAT,
-    total_snowfall FLOAT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date DATE NOT NULL,
+    precipitation FLOAT,
+    average_wind_speed FLOAT,
+    snowfall FLOAT,
+    sunrise TIME,
+    sunset TIME
 );
 
 CREATE TABLE IF NOT EXISTS taxi_trips (
-    pickup_datetime TEXT NOT NULL,
-    dropoff_datetime TEXT NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pickup_datetime DATETIME NOT NULL,
+    dropoff_datetime DATETIME NOT NULL,
     pu_lat FLOAT,
     pu_lon FLOAT,
     do_lat FLOAT,
     do_lon FLOAT,
     trip_miles FLOAT,
-    total_amount FLOAT
+    total_amount FLOAT,
+    fare_amount FLOAT,
+    extra FLOAT,
+    mta_tax FLOAT,
+    tip_amount FLOAT,
+    tolls_amount FLOAT,
+    improvement_surcharge FLOAT,
+    congestion_surcharge FLOAT,
+    airport_fee FLOAT
 );
 
 CREATE TABLE IF NOT EXISTS uber_trips (
-    pickup_datetime TEXT NOT NULL,
-    dropoff_datetime TEXT NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pickup_datetime DATETIME NOT NULL,
+    dropoff_datetime DATETIME NOT NULL,
     pu_lat FLOAT,
     pu_lon FLOAT,
     do_lat FLOAT,
     do_lon FLOAT,
     trip_miles FLOAT,
-    total_amount FLOAT
+    fare_amount FLOAT,
+    tolls_amount FLOAT,
+    bcf FLOAT,
+    sales_tax FLOAT,
+    tip_amount FLOAT,
+    airport_fee FLOAT,
+    congestion_surcharge FLOAT
 );
