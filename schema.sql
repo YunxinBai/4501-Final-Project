4a5da4d1ce3ee985911e8c261abcf9a1d0429150
YunxinBai <yb2609@columbia.edu>
2024-12-03 21:37:48 -0500

    Update the notebook and the repo

---
+++ schema.sql
@@ -0,0 +1,60 @@
+
+CREATE TABLE IF NOT EXISTS hourly_weather (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date DATETIME NOT NULL,
+    precipitation FLOAT,
+    wind_speed FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS daily_weather (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date DATE NOT NULL,
+    precipitation FLOAT,
+    average_wind_speed FLOAT,
+    snowfall FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS taxi_trips (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pickup_datetime DATETIME NOT NULL,
+    dropoff_datetime DATETIME NOT NULL,
+    pu_lat FLOAT,
+    pu_lon FLOAT,
+    do_lat FLOAT,
+    do_lon FLOAT,
+    trip_miles FLOAT,
+    total_amount FLOAT,
+    fare_amount FLOAT,
+    extra FLOAT,
+    mta_tax FLOAT,
+    tip_amount FLOAT,
+    tolls_amount FLOAT,
+    improvement_surcharge FLOAT,
+    congestion_surcharge FLOAT,
+    airport_fee FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS uber_trips (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pickup_datetime DATETIME NOT NULL,
+    dropoff_datetime DATETIME NOT NULL,
+    pu_lat FLOAT,
+    pu_lon FLOAT,
+    do_lat FLOAT,
+    do_lon FLOAT,
+    trip_miles FLOAT,
+    fare_amount FLOAT,
+    tolls_amount FLOAT,
+    bcf FLOAT,
+    sales_tax FLOAT,
+    tip_amount FLOAT,
+    airport_fee FLOAT,
+    congestion_surcharge FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS sun (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date DATE NOT NULL,
+    sunrise TIME,
+    sunset TIME
+);
